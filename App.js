58e0b79e2b5ecd6540fa025837866abcc2f2aec2
sheikhsuleman123 <mssheikh333@gmail.com>
2020-07-24 15:38:57 +0500

    drawer navigation and vector icon set OK

--- App.js
+++ App.js
@@ -1,21 +1,75 @@
 import React, { Component } from 'react';
 import { Text, View, Button } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import HomeScreen from    './screens/HomeScreen';
 import DetailsScreen from './screens/DetailsScreen';
 
+//Drawer Navigation 
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+// Stack Navigation 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+// Drawer Stack Screens
 
+const HomeStack = createStackNavigator();
+const DetailsStack = createStackNavigator();
+ 
+const HomeStackScreen = ({navigation}) => ( 
+      <HomeStack.Navigator screenOptions={{
+          headerStyle: {
+            backgroundColor: '#009387',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle : {
+            fontWeight:'bold'
+          }
+      }}>
+        <HomeStack.Screen name="Home"    component={HomeScreen} 
+        options = {{ headerLeft: () => (
+          <Icon.Button name="ios-menu" size={25} backgroundColor="#009387" 
+          onPress={() => navigation.openDrawer()} > </Icon.Button> )
+       }}   />
+      </HomeStack.Navigator> 
+   );
+   const DetailsStackScreen = ({navigation}) => ( 
+    <DetailsStack.Navigator screenOptions={{
+        headerStyle: {
+          backgroundColor: '#009387',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle : {
+          fontWeight:'bold'
+        }
+    }}>
+      <DetailsStack.Screen name="Detail"  component={DetailsScreen}
+       options = {{ headerLeft: () => (
+          <Icon.Button name="ios-menu" size={25} color="#fff" 
+          onPress={() => navigation.openDrawer()} > </Icon.Button> )
+       }}   />
+    </DetailsStack.Navigator> 
+ );
 
 
  class App extends Component {
   render() {
-    const Stack = createStackNavigator();
+
+    // const Stack = createStackNavigator();
+    const Drawer = createDrawerNavigator();
+
     return (
       <NavigationContainer>
-      <Stack.Navigator screenOptions={{
+
+  {/* Drawer Navigator  */} 
+       <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeStackScreen} />
+        <Drawer.Screen name="Deatil" component={DetailsStackScreen} />
+       </Drawer.Navigator>
+
+  {/* Stack Navigator */}
+      {/* <Stack.Navigator screenOptions={{
           headerStyle: {
             backgroundColor: '#009387',
           },
@@ -27,7 +81,8 @@ import { createStackNavigator } from '@react-navigation/stack';
         <Stack.Screen name="Home"    component={HomeScreen}    />
         <Stack.Screen name="Details" component={DetailsScreen} />
 
-      </Stack.Navigator>
+      </Stack.Navigator> */}
+
     </NavigationContainer>
     )
   }
